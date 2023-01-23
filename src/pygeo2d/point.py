b1c0d027f76a4c36b4d7740c9e37f4f590588f3d
Anthony Xu <63217938+ajxu2@users.noreply.github.com>
2023-01-22 20:05:28 -0600

    v0.0.1 Add Point class
    yes, it's the same thing as complex numbers

--- src/pygeo2d/point.py
+++ src/pygeo2d/point.py
@@ -1,4 +1,8 @@
 from numbers import Real
+from math import isfinite, sqrt
+
+def real_finite(x):
+    return isinstance(x, Real) and isfinite(x)
 
 class Point:
     """
@@ -15,13 +19,16 @@ class Point:
     >>> a += Point(3, 5)
     >>> print(a)
     (4, 7)
+    >>> a *= 5
+    >>> print(a)
+    (20, 35)
     """
 
     def __init__(self, x, y):
         """
         Create an instance of a Point.
         """
-        if not(isinstance(x, Real) and isinstance(y, Real)):
+        if not (real_finite(x) and real_finite(y)):
             raise TypeError("point coordinates must be real numbers")
         self.x = x
         self.y = y
@@ -42,18 +49,36 @@ class Point:
         """
         Multiply each coordinate of this point by a factor.
         """
-        if not isinstance(scale, Real):
+        if not real_finite(scale):
             raise TypeError("scale factor must be a real number")
         return Point(self.x * scale, self.y * scale)
     __rmul__ = __mul__
 
-    def __div__(self, scale):
+    def __truediv__(self, scale):
         """
         Divide each coordinate of this point by a factor.
         """
-        if not isinstance(scale, Real):
+        if not real_finite(scale):
             raise TypeError("scale factor must be a real number")
         return Point(self.x / scale, self.y / scale)
+
+    def __neg__(self):
+        """
+        Negate both coordinates of this point (180 degree rotation).
+        """
+        return Point(-self.x, -self.y)
+
+    def __pos__(self):
+        """
+        Return this point (???).
+        """
+        return self
+        
+    def __abs__(self):
+        """
+        Return the distance from this point to 0.
+        """
+        return sqrt(self.x * self.x + self.y * self.y)
 
     def __repr__(self):
         """
